34e3d7ac445272f6325f11b88e9fa048105d050a
testusr100 <smeo@gmx.de>
2016-07-23 14:02:01 +0200

    refactoring

--- cep-esper-playground/src/main/java/smeo/experiments/esper/priceaggregation/domain/event/PriceEvent.java
+++ cep-esper-playground/src/main/java/smeo/experiments/esper/priceaggregation/domain/event/PriceEvent.java
@@ -3,14 +3,14 @@ package smeo.experiments.esper.priceaggregation.domain.event;
 /**
  * Created by truehl on 22.07.16.
  */
-public class Price {
+public class PriceEvent {
 	private final long id;
 	private final String streamName;
 	private final String bank;
 	private final double bidPrice;
 	private final double askPrice;
 
-	public Price(long id, String streamName, String bank, double bidPrice, double askPrice) {
+	public PriceEvent(long id, String streamName, String bank, double bidPrice, double askPrice) {
 		this.id = id;
 		this.streamName = streamName;
 		this.bank = bank;
