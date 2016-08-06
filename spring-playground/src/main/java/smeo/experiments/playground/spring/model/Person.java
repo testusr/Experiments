5793b11572217a7819d048aaef8de6df7dc9bd97
testusr100 <smeo@gmx.de>
2016-08-06 12:02:30 +0200

    corrected classpath added missing attributes

--- spring-playground/src/main/java/smeo/experiments/playground/spring/model/Person.java
+++ spring-playground/src/main/java/smeo/experiments/playground/spring/model/Person.java
@@ -4,4 +4,31 @@ package smeo.experiments.playground.spring.model;
  * Created by truehl on 05.07.16.
  */
 public class Person {
+    private String name;
+    private String address;
+    private String age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getAge() {
+        return age;
+    }
 }
