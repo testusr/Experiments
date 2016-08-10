cc6d71784ba6491c14a6f5aa67f635eb67464b51
testusr <smeo@gmx.de>
2016-08-10 10:48:18 +0200

    adding read from chronicle queue example, removing *.iml files

--- chronicle-playground/src/main/java/smeo/experiments/chronicle/SimpleExample.java
+++ chronicle-playground/src/main/java/smeo/experiments/chronicle/SimpleExample.java
@@ -2,7 +2,9 @@ package smeo.experiments.chronicle;
 
 import net.openhft.chronicle.queue.ChronicleQueue;
 import net.openhft.chronicle.queue.ExcerptAppender;
+import net.openhft.chronicle.queue.ExcerptTailer;
 import net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder;
+import net.openhft.chronicle.wire.MethodReader;
 import smeo.experiments.chronicle.model.RiskMonitor;
 import smeo.experiments.chronicle.model.Side;
 import smeo.experiments.chronicle.model.TradeDetails;
@@ -19,13 +21,21 @@ public class SimpleExample {
 
     public static void main(String[] args) {
         ChronicleQueue queue = SingleChronicleQueueBuilder.binary(path + "/trades").build();
-            final ExcerptAppender appender = queue.acquireAppender();
-            // using the method writer interface.
-            RiskMonitor riskMonitor = appender.methodWriter(RiskMonitor.class);
-            final LocalDateTime now = LocalDateTime.now(Clock.systemUTC());
-            riskMonitor.trade(new TradeDetails(now, "GBPUSD", 1.3095, 10e6, Side.Buy, "peter"));
+        final ExcerptAppender appender = queue.acquireAppender();
+        // using the method writer interface.
+        RiskMonitor riskMonitor = appender.methodWriter(RiskMonitor.class);
+        final LocalDateTime now = LocalDateTime.now(Clock.systemUTC());
+        riskMonitor.trade(new TradeDetails(now, "GBPUSD", 1.3095, 10e6, Side.Buy, "peter"));
+
+        ChronicleQueue readingqueue = SingleChronicleQueueBuilder.binary(path + "/trades").build();
+        final ExcerptTailer tailer = readingqueue.createTailer();
+        // reading using method calls
+        RiskMonitor monitor = System.out::println;
+        MethodReader reader = tailer.methodReader(monitor);
+        // read one message
+        reader.readOne();
 
         // dump the content of the queue
-        System.out.println(queue.dump());
+        //System.out.println(queue.dump());
     }
 }
