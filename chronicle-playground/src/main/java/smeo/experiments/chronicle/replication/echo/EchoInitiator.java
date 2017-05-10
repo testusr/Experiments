6e168858759713754b2766a95c02827b7e52f67d
truehl <Quelle123>
2017-05-10 10:19:04 +0200

    na: added reports

--- chronicle-playground/src/main/java/smeo/experiments/chronicle/replication/echo/EchoInitiator.java
+++ chronicle-playground/src/main/java/smeo/experiments/chronicle/replication/echo/EchoInitiator.java
@@ -6,6 +6,8 @@ import net.openhft.chronicle.ExcerptAppender;
 import net.openhft.chronicle.ExcerptTailer;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class EchoInitiator {
@@ -14,8 +16,11 @@ public class EchoInitiator {
 	public static int ECHO_REFLECTOR_PORT = 12446;
 
 	public static final int NO_OF_ECHOS = 10000;
-	EchoData[] echos_send = new EchoData[NO_OF_ECHOS * 2];
 	EchoData[] echos_received = new EchoData[NO_OF_ECHOS];
+	long[] meanLatencyMs = new long[NO_OF_ECHOS];
+	long[] meanLatencyNanos = new long[NO_OF_ECHOS];
+
+	long echosPerSecond = 40000;
 
 	AtomicBoolean isWritingEchos = new AtomicBoolean(true);
 
@@ -55,10 +60,6 @@ public class EchoInitiator {
 			echos_received[i] = new EchoData();
 		}
 
-		for (int i = 0; i < echos_send.length; i++) {
-			echos_send[i] = new EchoData();
-		}
-
 		startReader(chronicle_in_path);
 		try {
 			System.out.println("waiting 10s to start writer");
@@ -67,24 +68,34 @@ public class EchoInitiator {
 			e.printStackTrace();
 		}
 
+		EchoData echo = new EchoData();
+
+		long nanoWaitTime = sendUpdateEveryXnanos(echosPerSecond);
 		while (true) {
 			System.out.println("start writing ");
 			int i = 0;
+			long nanosSendTime = -1;
+			long nextEchoSendTime = -1;
 			while (isWritingEchos.get()) {
-				int sendIndex = i % echos_send.length;
-				outgoingDataAppender.startExcerpt();
-				echos_send[sendIndex].echoCalled();
-				echos_send[sendIndex].writeExternal(outgoingDataAppender);
-				outgoingDataAppender.finish();
-				i++;
+				if (System.nanoTime() >= nextEchoSendTime) {
+					nanosSendTime = System.nanoTime();
+					nextEchoSendTime = nanosSendTime + nanoWaitTime;
+					outgoingDataAppender.startExcerpt();
+					echo.newEchoCall();
+					echo.writeExternal(outgoingDataAppender);
+					outgoingDataAppender.finish();
+					i++;
+				}
 			}
 			System.out.println("writing stopped after " + i + " echos");
-
-			refurbishPreallocatedSendEchos();
 			waitTillWritingEnabledAgain();
 
 		}
 
+	}
+
+	private long sendUpdateEveryXnanos(long echosPerSecond) {
+		return (TimeUnit.SECONDS.toNanos(1) / echosPerSecond);
 	}
 
 	private void waitTillWritingEnabledAgain() {
@@ -94,12 +105,6 @@ public class EchoInitiator {
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-		}
-	}
-
-	private void refurbishPreallocatedSendEchos() {
-		for (int j = 0; j < echos_send.length; j++) {
-			echos_send[j].refurbish();
 		}
 	}
 
@@ -124,18 +129,22 @@ public class EchoInitiator {
 
 		public void start() {
 			new Thread(new Runnable() {
+				EchoData dummyEcho = new EchoData();
+
 				@Override
 				public void run() {
 					while (true) {
 						System.out.println("respawning read");
 						// giving it a few packages to establish the flow
-						int i = -1000;
+						final int warmupEchos = 1000;
+						int i = -warmupEchos;
 						isWritingEchos.set(true);
 						while (i < echos_received.length) {
 							if (tailer.nextIndex()) {
 								try {
 									if (i >= 0) {
 										echos_received[i].readExternal(tailer);
+										echos_received[i].relectionReceived();
 									}
 									i++;
 								} catch (IOException e) {
@@ -146,10 +155,29 @@ public class EchoInitiator {
 							}
 						}
 
-						System.out.println("enough samples read (" + i + ") ");
+						System.out.println("enough samples read (" + i + ") + '" + warmupEchos + "' warmup echos");
 						isWritingEchos.set(false);
 						printReport(echos_received);
+						waitTillNoEchosAreReceivedAnymoreForSec(3);
 						tailer.toEnd();
+					}
+				}
+
+				private void waitTillNoEchosAreReceivedAnymoreForSec(int waitTimeInSec) {
+					System.out.println("wait until no echos received for '" + waitTimeInSec + "' in sec");
+					long lastEchoReceived = System.currentTimeMillis();
+					long waitTimeInMs = TimeUnit.SECONDS.toMillis(waitTimeInSec);
+					while ((System.currentTimeMillis() - lastEchoReceived) < waitTimeInMs) {
+						if (tailer.nextIndex()) {
+							lastEchoReceived = System.currentTimeMillis();
+							try {
+								dummyEcho.readExternal(tailer);
+							} catch (IOException e) {
+								e.printStackTrace();
+							} catch (ClassNotFoundException e) {
+								e.printStackTrace();
+							}
+						}
 					}
 				}
 			}).start();
@@ -157,6 +185,34 @@ public class EchoInitiator {
 	}
 
 	private void printReport(EchoData[] echos_received) {
+		long missingEchos = 0;
+		long doubledEchos = 0;
+		for (int i = 1; i < echos_received.length; i++) {
+			long idDiff = echos_received[i].id - echos_received[i - 1].id;
+			if (idDiff == 0) {
+				doubledEchos++;
+			}
+			if (idDiff > 1) {
+				missingEchos += (idDiff - 1);
+			}
+		}
+		for (int i = 0; i < echos_received.length; i++) {
+			EchoData currEcho = echos_received[i];
+			meanLatencyMs[i] = currEcho.meanLatencyMs();
+			meanLatencyNanos[i] = currEcho.meanLatencyNanos();
+		}
+		Arrays.sort(meanLatencyMs);
+		Arrays.sort(meanLatencyNanos);
+
+		StringBuilder reportBuilder = new StringBuilder();
+		reportBuilder.append("missingEchos: ").append(missingEchos).append("\n");
+		reportBuilder.append("doubledEchos: ").append(doubledEchos).append("\n");
+		reportBuilder.append("99th perc. mean ms: ").append(meanLatencyMs[(meanLatencyMs.length / 100) * 99]).append("\n");
+		reportBuilder.append("99.9th perc. mean ms: ").append(meanLatencyMs[(int) Math.round((meanLatencyMs.length / 100) * 99.9)]).append("\n");
+		reportBuilder.append("99th perc. mean nanos: ").append(meanLatencyNanos[(meanLatencyNanos.length / 100) * 99]).append("\n");
+		reportBuilder.append("99.9th perc. mean nanos: ").append(meanLatencyNanos[(int) Math.round((meanLatencyMs.length / 100) * 99.9)]).append("\n");
+
+		System.out.println(reportBuilder.toString());
 	}
 
 	public static void main(String[] args) {
